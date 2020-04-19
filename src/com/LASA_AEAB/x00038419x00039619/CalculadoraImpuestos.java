612561d4122a3a9414b69a5b8638c513ac34afd2
Ashiock <51394301+Ashiock@users.noreply.github.com>
2020-04-19 16:38:21 -0600

    Hola Leo meperdonas¿

---
+++ src/com/LASA_AEAB/x00038419x00039619/CalculadoraImpuestos.java
@@ -0,0 +1,20 @@
+package com.LASA_AEAB.x00038419x00039619;
+
+public static class CalculadoraImpuestos {
+
+    private static double totalRenta;
+    private static double totalISS;
+    private static double totalAFP;
+
+    CalculadoraImpuestos(){
+
+    }
+
+    public static double calcularPago(Empleado){
+
+    }
+
+    public static String mostrarTotales(){
+
+    }
+}
---
+++ src/com/LASA_AEAB/x00038419x00039619/PlazaFija.java
@@ -0,0 +1,19 @@
+package com.LASA_AEAB.x00038419x00039619;
+
+public class PlazaFija extends Empleado {
+    private int extension;
+
+    PlazaFija(String nombre, String puesto, double salario, int meses){
+        super(nombre, puesto, salario);
+        this.extension = extension;
+    }
+
+    public int getExtension() {
+        return extension;
+    }
+
+    public void setExtension(int extension) {
+            this.extension = extension;
+    }
+
+}
